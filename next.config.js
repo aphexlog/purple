db4028e55ed688c8b167e1c1189c273a9e2150d3
Billy <aphexlog@gmail.com>
2022-07-31 13:47:18 +0000

    adding unoptimized stanza to next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+      // allowFutureImage: true
+    }
+  }
 }
 
-module.exports = {
-  nextConfig,
-  images: {
-    loader: 'imgix',
-    path: '',
-  },
-}
+module.exports = nextConfig
