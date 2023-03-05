797c19eb1d7276fc1f1311b4df2ac0f18fbed6ea
billy <aphexlog@gmail.com>
2023-03-04 18:44:40 -0600

    fixing some logical resource IDs so that they make sense

--- lambda/main.tf
+++ lambda/main.tf
@@ -6,7 +6,7 @@ resource "aws_internet_gateway" "super_awesome_igw" {
     vpc_id = aws_vpc.super_awesome_vpc.id
 }
 
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.super_awesome_vpc.id
     cidr_block = "10.0.16.0/20"
 }
@@ -20,31 +20,31 @@ resource "aws_route_table" "super_awesome_route_table_public" {
 }
 
 resource "aws_route_table_association" "super_awesome_route_table_association_public" {
-    subnet_id = aws_subnet.subnet1.id
+    subnet_id = aws_subnet.public_subnet.id
     route_table_id = aws_route_table.super_awesome_route_table_public.id
 }
 
 resource "aws_route_table" "super_awesome_route_table_private" {
     vpc_id = aws_vpc.super_awesome_vpc.id
     route {
-        cidr_block = aws_subnet.subnet2.cidr_block
+        cidr_block = aws_subnet.private_subnet.cidr_block
         nat_gateway_id = aws_nat_gateway.super_awesome_nat.id
     }
 }
 
 resource "aws_route_table_association" "super_awesome_route_table_association_private" {
-    subnet_id = aws_subnet.subnet2.id
+    subnet_id = aws_subnet.private_subnet.id
     route_table_id = aws_route_table.super_awesome_route_table_private.id
 }
 
 resource "aws_eip" "super_awesome_eip" {}
 
 resource "aws_nat_gateway" "super_awesome_nat" {
-    subnet_id = aws_subnet.subnet1.id
+    subnet_id = aws_subnet.public_subnet.id
     allocation_id = aws_eip.super_awesome_eip.id
 }
 
-resource "aws_subnet" "subnet2" {
+resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.super_awesome_vpc.id
     cidr_block = "10.0.128.0/20"
 }
@@ -54,7 +54,7 @@ resource "aws_security_group" "lambda_sg" {
     vpc_id = aws_vpc.super_awesome_vpc.id
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
+resource "aws_iam_role" "lambda_role" {
     assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -93,21 +93,21 @@ resource "aws_iam_policy" "aws_iam_role_policy" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "attach_policy" {
-    role       = aws_iam_role.iam_for_lambda.name
+resource "aws_iam_role_policy_attachment" "attach_logging_policy" {
+    role       = aws_iam_role.lambda_role.name
     policy_arn = aws_iam_policy.aws_iam_role_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "attach_policy2" {
-    role       = aws_iam_role.iam_for_lambda.name
+resource "aws_iam_role_policy_attachment" "attach_lambda_vpv_policy" {
+    role       = aws_iam_role.lambda_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_lambda_function" "awesome_function" {
     function_name = "awesome_function"
-    role          = aws_iam_role.iam_for_lambda.arn
+    role          = aws_iam_role.lambda_role.arn
     vpc_config {
-        subnet_ids = [aws_subnet.subnet1.id]
+        subnet_ids = [aws_subnet.public_subnet.id]
         security_group_ids = [aws_security_group.lambda_sg.id]
     }
     runtime = "nodejs16.x"
